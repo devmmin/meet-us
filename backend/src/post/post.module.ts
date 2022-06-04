b219ed777b04630a4e69ae814616c9c6d651127c
ByeongGi <wwww3426@naver.com>
2022-06-04 19:02:29 +0900

    feat(BE): #30 Post Moudule 디펜던시 수정

--- backend/src/post/post.module.ts
+++ backend/src/post/post.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostResolver } from '@post/resolvers/post.resolver';
+import { PrismaModule } from '@prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [PostResolver],
 })
 export class PostModule {}
